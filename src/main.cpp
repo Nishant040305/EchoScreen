0dcb6493d75834be5bed01d64d90dfe21ed6184e
mukund-gpt <abhishek.jupyter@gmail.com>
2025-09-05 19:11:33 +0530

    chore: update the board and settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,18 @@
-#include <WiFi.h>
+#include <Arduino.h>
 
-const char* ssid = "Blaze Brij mohan srivastava-5G";
-const char* pass = "110806ac";
-
-WiFiServer server(80);
+// put function declarations here:
+int myFunction(int, int);
 
 void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, pass);
-
-  Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("\nConnected!");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-
-  server.begin();
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
 }
 
 void loop() {
-  WiFiClient client = server.available();   // listen for incoming clients
-  if (client) {
-    Serial.println("New Client connected.");
-    String request = "";
+  // put your main code here, to run repeatedly:
+}
 
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        request += c;
-
-        // End of request
-        if (c == '\n') {
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-type:text/html");
-          client.println("Connection: close");
-          client.println();
-          client.println("<!DOCTYPE html><html>");
-          client.println("<h1>Hello from ESP32!</h1>");
-          client.println("</html>");
-          break;
-        }
-      }
-    }
-    delay(1);
-    client.stop();
-    Serial.println("Client disconnected.");
-  }
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
 }
