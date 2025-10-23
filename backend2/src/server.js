5450d96d7c7c0d4738fcf3797be0bae1570d5700
Nishant <nishant040305@gmail.com>
2025-10-23 15:06:39 +0530

    feat: add landing page for the backend

--- backend2/src/server.js
+++ backend2/src/server.js
@@ -34,7 +34,9 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Internal Server error");
 });
-
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server and Socket.io are running on port ${PORT}`);
